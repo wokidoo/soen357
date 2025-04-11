007a65cef0978df5c963b41535262ab8874c614b
peaches <peizhe2001@hotmail.com>
2025-04-10 22:42:27 -0400

    change some articles

--- lib/database/article_database.dart
+++ lib/database/article_database.dart
@@ -2,7 +2,7 @@ class ArticleDatabase {
   static List<String> articleUrls = [
     'https://www.thesill.com/blogs/plants-101/top-ten-plant-care-tips',
     'https://theeverygirl.com/indoor-plant-care/',
-    'https://www.pickuplimes.com/article/houseplant-care-tips-for-beginners-99',
+    'https://www.thrive.org.uk/get-gardening/caring-for-houseplants',
     'https://mortonarb.org/blog/proper-watering-keeps-plants-healthy/',
   ];
 }
